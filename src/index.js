1b34563580bebdd67c4e18488d811e12b7daad6c
yugo_i <u15jpn@gmail.com>
2020-11-19 23:58:32 +0900

    impolement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import _ from "lodash";
+import Tiger, { NAME as NAME_OF_U5 } from "./utilities";
+
+// console.log(utilities.Nijou(3));
+console.log(NAME_OF_U5);
+console.log(Tiger.say());
 
 function component() {
   const element = document.createElement("div");
